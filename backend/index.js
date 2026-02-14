98f69689a847b439e531125c32a91574fc04b87f
bansi957 <bansibasha@gmail.com>
2026-02-14 12:18:54 +0530

    frontend url changed

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const server=createServer(app)
 
 
 const io=new Server(server,{cors:{
-    origin:"http://localhost:5173/",
+    origin:"https://upnext-dzd9.onrender.com",
     credentials:true,
     methods:['POST','GET','PUT','DELETE']
 }})
@@ -23,7 +23,7 @@ const io=new Server(server,{cors:{
 
 app.set("io",io)
 app.use(cors({
-    origin:"http://localhost:5173/",
+    origin:"https://upnext-dzd9.onrender.com",
     credentials:true
 }))
 app.use(express.json())
